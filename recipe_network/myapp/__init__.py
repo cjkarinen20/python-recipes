5493e76e213da3e4952b91796a19a54b47afb6c8
cjkarinen20 <cjkarinen20@gmail.com>
2026-03-24 14:17:55 -0400

    Added User Model
    -Added the User Model.
    -Reset the database to fix a migration error.
    -Pushed a new migration for the User and Recipe
    models.

--- recipe_network/myapp/__init__.py
+++ recipe_network/myapp/__init__.py
@@ -2,11 +2,16 @@ import os
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
-app.config['SECRET_KEY'] = os.getenv("S1ECRET_KEY")
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
+migrate.init_app(app, db, render_as_batch=True)
+
+
+login_manager = LoginManager(app)
 from myapp import routes, models
