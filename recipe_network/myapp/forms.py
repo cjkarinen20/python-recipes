3b6174589d4896935521101e6b62b1f56a9da460
cjkarinen20 <cjkarinen20@gmail.com>
2026-03-24 16:53:26 -0400

    Created User Registration Form
    -Created the User Registration Form in which
    user accounts are created.

--- recipe_network/myapp/forms.py
+++ recipe_network/myapp/forms.py
@@ -1,8 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from .models import User
 
 class RecipeForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     description = TextAreaField('Description', validators=[DataRequired()])
     submit = SubmitField('Post')
+    
+class RegistrationForm(FlaskForm):
+    username = StringField('Title', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Email()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+    
+    def validate_username(self, username):
+        user = User.query.filter_by(username = username)
+        if user:
+            raise ValidationError('Please use a different username.')
+    def validate_email(self, email):
+        user = User.query.filter_by(email = email.data).first()
+        if user:
+            raise ValidationError('Please use a different email address.')
+        
