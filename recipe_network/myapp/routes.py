93a43b2e6df3ad1789585da76f2177678efb1e2a
cjkarinen20 <cjkarinen20@gmail.com>
2026-03-24 18:18:43 -0400

    Added Sharing Model to Database
    -Added the recipe sharing model to the database.
    -Created a new database migration.
    -Added the route for '/share_recipe/'.

--- recipe_network/myapp/routes.py
+++ recipe_network/myapp/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, redirect, url_for
-from .models import Recipe, User, check_password_hash
+from flask import render_template, redirect, url_for, request
+from .models import Recipe, User, check_password_hash, SharedRecipe
 from .forms import RecipeForm, RegistrationForm, LoginForm
 from . import app, db
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 
 @app.route('/')
 def home():
@@ -52,4 +52,19 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+@app.route('/share_recipe/<int:recipe_id>', methods = ['GET', 'POST'])
+@login_required
+def share_recipe(recipe_id):
+    recipe = Recipe.query.get_or_404(recipe_id)
+    users = User.query.filter(User.id != current_user.id).all()
+    if request.method == "POST":
+        selected_user_id = request.form.get('sharee')
+        sharee = User.query.get(selected_user_id)
+        if sharee:
+            shared_recipe = SharedRecipe(recipe_id = recipe.id, sharer_id = current_user.id, sharee_id = sharee.id)
+            db.session.add(shared_recipe)
+            db.session.commit()
+            return redirect(url_for('home'))
+        return render_template('share_recipe.html', recipe = recipe, users = users)
         
